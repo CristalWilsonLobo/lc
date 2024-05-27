f9982d21f4e0d6253a3f8d86fd8eea395fa607dc
Cristal Wilson Lobo <143117888+CristalWilsonLobo@users.noreply.github.com>
2024-05-27 19:33:38 -0400

    Update stock_profit.java

--- src/main/java/stock_profit.java
+++ src/main/java/stock_profit.java
@@ -1,1 +1,50 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+/*
+        int buy=prices.length,sell=1, profit = 0, min_buy = Integer.MAX_VALUE , max_sell = 0;
+        while(buy>=0 && sell < prices.length && buy<sell){
+            min_buy = Math.min(min_buy,prices[buy]);
+            max_sell = Math.max(max_sell,prices[sell]);
+            
+            profit=Math.max(profit,prices[sell]-prices[buy]);
+            buy--;
+        }
+    return profit;
+*/
+    	
+        int currentProfit = 0;
+        int maxProfit = 0;
 
+        for(int i=0;i<prices.length-1;i++)
+        {
+            int buy = prices[i];
+            int temp[] = Arrays.copyOfRange(prices, (i+1), prices.length);
+            Arrays.sort(temp);
+            int sell = temp[temp.length-1];
+            currentProfit = sell - buy;
+            if(currentProfit>maxProfit)
+            {
+                maxProfit = currentProfit;
+            }
+        }
+        return maxProfit;
+        /*
+        int buy = Ints.indexOf(prices,temp[0]);
+        int sell =0;
+        if(buy==prices.length-1)
+        {
+        	return 0;
+        }
+        else
+        {
+        	int temp1[] = Arrays.copyOfRange(prices, buy, prices.length );
+        	sell = Ints.indexOf(prices,temp1[prices.length-1]);
+        }
+        if(sell-buy > 0)
+        {
+        	return sell-buy;
+        }
+        return 0;
+    	*/
+    }
+}
